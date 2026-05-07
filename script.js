028d2ed0e2ef3efbbd1ac7f48cc7d4f1b07a77ed
sophdiaz0504-cloud <soph.diaz0504@gmail.com>
2026-05-07 12:34:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+let darkMode = false;
+ 
+if (darkMode) {
+  document.body.style.background = "black";
+  document.body.style.color = "white";
+} else {
+  document.body.style.background = "white";
+  document.body.style.color = "black";
+}
