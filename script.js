22c52c7f48ac9892e892ebd753633a8009580526
sophdiaz0504-cloud <soph.diaz0504@gmail.com>
2026-05-07 12:40:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ if (darkMode) {
   document.body.style.background = "white";
   document.body.style.color = "black";
 }
+const body = document.body;
+const button = document.getElementById("themeToggle");
+ 
+button.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
+const gallery = document.getElementById("gallery");
+gallery.addEventListener("mouseover", () => {
+  gallery.src = "image2.jpg";
+});
+gallery.addEventListener("mouseout", () => {
+  gallery.src = "image1.jpg";
+});
+</script>
